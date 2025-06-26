24b5579262a24decd1f5d9214fe6348342a20754
Gunja Prajapati <gunjaprajapati23@gmail.com>
2025-06-26 17:13:51 +0530

    final change

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     role = db.Column(db.String(20), default='user')  # 'admin' or 'user'
 
     def set_password(self, password):
